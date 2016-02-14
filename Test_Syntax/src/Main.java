30e73d2f0fc475764aec824fd55728303fa3a02a
radua <radua@j2inn.com>
2016-02-14 21:54:44 +0200

    Am incercat sa fac ceva cu Templates. dar nu am reusit.

--- Test_Syntax/src/Main.java
+++ Test_Syntax/src/Main.java
@@ -1,12 +1,12 @@
 import testDispatch.TestDispatch;
 import testMisc.TestMisc;
+import testTemplate.TestTemplate;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    TestMisc.test();
-    
+    TestTemplate.test();
     System.out.println("Done!");
   }
 }
